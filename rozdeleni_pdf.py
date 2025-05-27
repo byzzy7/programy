64a29600af5da5d9ee6909095bd7cad704137fad
Lukáš Bystroň <lbystron@gmail.com>
2025-05-27 15:36:26 +0200

    Add files via upload

--- rozdeleni_pdf.py
+++ rozdeleni_pdf.py
@@ -7,15 +7,16 @@ from PyPDF2 import PdfReader, PdfWriter
 import datetime
 import re
 
-with open("nazev.pdf", "rb") as file:
+with open("pasky_04.pdf", "rb") as file:
     reader = PdfReader(file)
     # Získání aktuálního data ve formátu MM.YYYY
     datum = datetime.datetime.today().strftime('%m.%Y')
 
     for i, page in enumerate(reader.pages):
         text = page.extract_text()
-        nadpis = text[80:96] if text and len(text) >= 96 else f'strana_{i + 1}'
-        nadpis = re.sub(r'[<>:"/\\|?*]', '', nadpis)  # nahrazení zakázaných znaků
+        # Nadpis stránky je extrahován z textu, pozice 74 až 96
+        nadpis = text[74:96]
+        nadpis = re.sub(r'[<>:"/\\|?*]', '', nadpis) # nahrazení zakázaných znaků
         filename = f'{nadpis}- {datum}.pdf'
         writer = PdfWriter()
         writer.add_page(page)
