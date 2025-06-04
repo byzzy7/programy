cc61c02d746edbad45968f2814143bf687072b21
Lukáš Bystroň <lbystron@gmail.com>
2025-06-04 21:33:15 +0200

    Add files via upload

--- rozdeleni_pdf.py
+++ rozdeleni_pdf.py
@@ -1,25 +1,27 @@
-'''
-author: Lukáš Bystroň
-email: lbystron@gmail.com
-'''
-
 from PyPDF2 import PdfReader, PdfWriter
 import datetime
 import re
 
-with open("nazev.pdf", "rb") as file:
-    reader = PdfReader(file)
-    # Získání aktuálního data ve formátu MM.YYYY
-    datum = datetime.datetime.today().strftime('%m.%Y')
+nazev_souboru = "xxxxx.pdf"
 
-    for i, page in enumerate(reader.pages):
-        text = page.extract_text()
-        # Nadpis stránky je extrahován z textu, pozice 74 až 96
-        nadpis = text[74:96]
-        nadpis = re.sub(r'[<>:"/\\|?*]', '', nadpis) # nahrazení zakázaných znaků
-        filename = f'{nadpis}- {datum}.pdf'
-        writer = PdfWriter()
-        writer.add_page(page)
-        with open(filename, 'wb') as output_file:
-            writer.write(output_file)
-            print(f'Vytvořen soubor: {filename}')
+try:
+    with open(nazev_souboru, "rb") as file:
+        reader = PdfReader(file)
+        # Získání aktuálního data ve formátu MM.YYYY
+        datum = datetime.datetime.today().strftime('%m.%Y')
+
+        for i, page in enumerate(reader.pages):
+            text = page.extract_text()
+            # Nadpis stránky je extrahován z textu, pozice 74 až 96
+            nadpis = text[74:96]
+            nadpis = re.sub(r'[<>:"/\\|?*]', '', nadpis) # nahrazení zakázaných znaků
+            filename = f'{nadpis}.pdf'
+            writer = PdfWriter()
+            writer.add_page(page)
+            with open(filename, 'wb') as output_file:
+                writer.write(output_file)
+                print(f'Vytvořen soubor: {filename}')
+except FileNotFoundError:
+    print(f"Soubor {nazev_souboru} nebyl nalezen.")
+except Exception as e:
+    print(f"Došlo k chybě: {e}")
